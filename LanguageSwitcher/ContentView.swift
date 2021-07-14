e21f9b7fd50691ac3122e2c9acf7a3aa04353d35
Fan Wu <fanwu@pm.me>
2021-07-14 21:48:33 +0800

    Initial Commit

---
+++ LanguageSwitcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LanguageSwitcher
+//
+//  Created by 范武 on 2021/7/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LanguageSwitcher/LanguageSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageSwitcherApp.swift
+//  LanguageSwitcher
+//
+//  Created by 范武 on 2021/7/14.
+//
+
+import SwiftUI
+
+@main
+struct LanguageSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
