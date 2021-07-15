1af60ba91e88dd9d321456e44043f10e73e51f2f
Fan Wu <fanwu@pm.me>
2021-07-15 11:34:30 +0800

    menu bar

--- LanguageSwitcher/LanguageSwitcherApp.swift
+++ LanguageSwitcher/LanguageSwitcherApp.swift
@@ -9,9 +9,48 @@ import SwiftUI
 
 @main
 struct LanguageSwitcherApp: App {
+    //connect to App Delegate
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate{
+    //status bar item
+    var statusItem: NSStatusItem?
+    
+    //Popover
+    var popOver = NSPopover()
+    func applicationDidFinishLaunching(_ notificaiton:Notification) {
+        // Menu View
+        let menuView = ContentView()
+        popOver.behavior = .transient
+        popOver.animates = true
+        popOver.contentViewController = NSHostingController(rootView: menuView)
+        popOver.contentSize = NSSize(width: 500, height: 500)
+        
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        //safe check isf status Button is Available or not
+        if let menuButton = statusItem?.button{
+            menuButton.image = NSImage(named: "icon")
+            menuButton.action = #selector(togglePopover(_:))
+            
+        }
+        
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject){
+        if let button = statusItem?.button{
+            if popOver.isShown{
+                popOver.performClose(sender)
+            }else{
+                popOver.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
         }
     }
 }
